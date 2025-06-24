d9dde47a50e54f8c0fc194cffc4bf81ad8176bc3
zh9un <134996148+zh9un@users.noreply.github.com>
2025-06-24 18:17:29 +0900

    Title: 숫자로 이루어진 사각형, Time: 83ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250624/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+# Please write your code here.
+def a() :
+    for _ in range (n) :
+        for _ in range (n) :
+            
+            print ( count , ' ')
+            if count > 9 :
+                count = 1
+        print ()
+    
+a()
